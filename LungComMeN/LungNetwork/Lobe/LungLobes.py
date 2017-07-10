524fd7b7931183ef9c1c32a81342a019e2b71f77
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-07-10 15:16:53 +0100

    Split out lung lobe IDs

---
+++ LungComMeN/LungNetwork/Lobe/LungLobes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""Short docstring
+
+Long Docstring
+
+"""
+
+# imports
+
+__author__ = "Michael Pitcher"
+__copyright__ = "Copyright 2017"
+__credits__ = ["Michael Pitcher"]
+__license__ = ""
+__version__ = ""
+__email__ = "mjp22@st-andrews.ac.uk"
+__status__ = "Development"
+
+SUPERIOR_RIGHT = 'superior_right'
+MIDDLE_RIGHT = 'middle_right'
+INFERIOR_RIGHT = 'inferior_right'
+SUPERIOR_LEFT = 'superior_left'
+INFERIOR_LEFT = 'inferior_left'
