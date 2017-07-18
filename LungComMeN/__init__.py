c8d03a87d358937cfce4fdc0566cafd4e77597b7
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-07-18 15:16:46 +0100

    Renames

--- LungComMeN/__init__.py
+++ LungComMeN/__init__.py
@@ -16,9 +16,8 @@ __version__ = ""
 __email__ = "mjp22@st-andrews.ac.uk"
 __status__ = "Development"
 
-from LungComMeN.LungDynamics.LungCompartments import *
-from LungDynamics import *
-from LungEvents import *
+from LungTBDynamics import *
+from LungTBEvents import *
 from LungNetwork import *
 from LungVisuals import *
 from ComMeN import *
