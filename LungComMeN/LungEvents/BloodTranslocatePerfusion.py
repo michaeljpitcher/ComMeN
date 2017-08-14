a53f226f55a7fda63a49be50fcddaa8a093175a9
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-08-14 10:53:40 +0100

    Translocate through blood based on perfusion

--- LungComMeN/LungEvents/BloodTranslocatePerfusion.py
+++ LungComMeN/LungEvents/BloodTranslocatePerfusion.py
@@ -6,7 +6,8 @@ Long Docstring
 
 """
 
-# imports
+from ComMeN.Events.Translocate import *
+from ..LungNetwork.BloodEdge import *
 
 __author__ = "Michael Pitcher"
 __copyright__ = "Copyright 2017"
@@ -16,3 +17,23 @@ __version__ = ""
 __email__ = "mjp22@st-andrews.ac.uk"
 __status__ = "Development"
 
+
+class BloodTranslocatePerfusion(Translocate):
+
+    def __init__(self, reaction_parameter, nodes, compartment_translocating):
+        Translocate.__init__(self, reaction_parameter, nodes, compartment_translocating, edge_class=BloodEdge,
+                             rate_increases_with_edges=False)
+
+    def _pick_edge(self, edges):
+        """
+        Pick an edge, probabilistically based on perfusion
+        :param edges: Edges to choose from
+        :return: Edge chosen
+        """
+        total_perfusion = sum([e.perfusion for e in edges])
+        r = rand.random() * total_perfusion
+        running_total = 0
+        for e in edges:
+            running_total += e.perfusion
+            if running_total >= r:
+                return e
