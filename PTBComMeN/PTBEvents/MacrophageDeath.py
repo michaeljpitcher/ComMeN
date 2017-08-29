2c2ae5fa19d09b176448c5cc648f285d671655a3
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-08-29 11:17:02 +0100

    Only one influencer per event

--- PTBComMeN/PTBEvents/MacrophageDeath.py
+++ PTBComMeN/PTBEvents/MacrophageDeath.py
@@ -25,22 +25,25 @@ def get_macrophage_death_events(nodes, standard_death_rates, death_by_t_cell_rat
         # TODO - what to do with intracell during natural death? Currently they die
         events.append(MacrophageDeath(standard_death_rates[m], nodes, m))
     # By T-cell
-    events.append(MacrophageDeath(death_by_t_cell_rate, nodes, MACROPHAGE_INFECTED, [T_CELL_ACTIVATED]))
+    events.append(MacrophageDeath(death_by_t_cell_rate, nodes, MACROPHAGE_INFECTED, T_CELL_ACTIVATED))
     # By bursting
-    events.append(MacrophageDeath(death_bursting_rate, nodes, MACROPHAGE_INFECTED, [BACTERIUM_INTRACELLULAR], False))
+    events.append(MacrophageDeath(death_bursting_rate, nodes, MACROPHAGE_INFECTED, BACTERIUM_INTRACELLULAR, False))
     return events
 
 
 class MacrophageDeath(Destroy):
-    def __init__(self, reaction_parameter, nodes, compartment_destroyed, influencing_compartments=None,
+    def __init__(self, reaction_parameter, nodes, compartment_destroyed, influencing_compartment=None,
                  destroy_bacteria=True):
         self._destroy_bacteria = destroy_bacteria
-        Destroy.__init__(self, reaction_parameter, nodes, compartment_destroyed, influencing_compartments)
+        if influencing_compartment:
+            Destroy.__init__(self, reaction_parameter, nodes, compartment_destroyed, [influencing_compartment])
+        else:
+            Destroy.__init__(self, reaction_parameter, nodes, compartment_destroyed)
 
     def _update_node(self, node):
         changes = {self._compartment_destroyed: -1}
         if self._compartment_destroyed == MACROPHAGE_INFECTED:
-            bacteria_inside = int(round(node[self._compartment_destroyed] * 1.0 / node[BACTERIUM_INTRACELLULAR]))
+            bacteria_inside = int(round(float(node[BACTERIUM_INTRACELLULAR]) / node[self._compartment_destroyed]))
             changes[BACTERIUM_INTRACELLULAR] = -1 * bacteria_inside
             if not self._destroy_bacteria:
                 changes[BACTERIUM_SLOW] = bacteria_inside
