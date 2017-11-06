baae96d9b77e110e095521f1ea366545cf0e85ef
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-11-06 11:50:09 +0000

    No need to specify t-cell type

--- Test_PTBComMeN/Events/Test_TCellTranslocation.py
+++ Test_PTBComMeN/Events/Test_TCellTranslocation.py
@@ -8,7 +8,7 @@ class TCellTranslocationBloodTestCase(unittest.TestCase):
         self.nodes = [LymphPatch(1, compartments), LungPatch(0, compartments, 0.9, 0.3)]
         self.edges = [BloodEdge(self.nodes[0], self.nodes[1])]
         self.network = MetapopulationNetwork(self.nodes, self.edges)
-        self.event = TCellTranslocationBlood(0.1, self.nodes, T_CELL_ACTIVATED)
+        self.event = TCellTranslocationBlood(0.1, self.nodes)
         uh = UpdateHandler([self.event])
 
     def test_rate(self):
@@ -33,8 +33,8 @@ class GetTCellTranslocationEventsTestCase(unittest.TestCase):
     def setUp(self):
         compartments = [T_CELL_ACTIVATED]
         self.nodes = [LymphPatch(0, compartments)]
-        self.rates = {T_CELL_ACTIVATED: 0.1}
-        self.events = get_t_cell_translocation_events(self.nodes, self.rates)
+        self.rate = 0.1
+        self.events = get_t_cell_translocation_events(self.nodes, self.rate)
 
     def test_events(self):
         self.assertEqual(len(self.events), 1)
