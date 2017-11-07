2ef22d6d380d8617e382be3debf7dd9a037ead2c
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-11-07 16:01:27 +0000

    Lymphangitis tests

--- Test_PTBComMeN/Events/Test_BacteriaTranslocationBlood.py
+++ Test_PTBComMeN/Events/Test_BacteriaTranslocationBlood.py
@@ -27,6 +27,31 @@ class BacteriaTranslocationBloodTestCase(unittest.TestCase):
         self.assertEqual(self.nodes[1][BACTERIUM_EXTRACELLULAR_FAST], 2)
 
 
+class BacteriaTranslocateLymphangitisTestCase(unittest.TestCase):
+    def setUp(self):
+        self.nodes = [LymphPatch(1, ALL_TB_COMPARTMENTS), LungPatch(0, ALL_TB_COMPARTMENTS, 0.9, 0.3)]
+        self.edges = [BloodEdge(self.nodes[0], self.nodes[1]), LymphEdge(self.nodes[1], self.nodes[0], 3)]
+        self.network = MetapopulationNetwork(self.nodes, self.edges)
+        self.event = BacteriaTranslocateLymphangitis(0.1, self.nodes, BACTERIUM_EXTRACELLULAR_FAST)
+        uh = UpdateHandler([self.event])
+
+    def test_rate(self):
+        self.assertEqual(self.event.rate, 0)
+        self.nodes[0].update({BACTERIUM_EXTRACELLULAR_FAST: 3})
+        self.assertEqual(self.event.rate, 0.1 * 3)
+
+    def test_update(self):
+        self.nodes[0].update({BACTERIUM_EXTRACELLULAR_FAST: 3})
+        self.assertEqual(self.nodes[0][BACTERIUM_EXTRACELLULAR_FAST], 3)
+        self.assertEqual(self.nodes[1][BACTERIUM_EXTRACELLULAR_FAST], 0)
+        self.event.perform()
+        self.assertEqual(self.nodes[0][BACTERIUM_EXTRACELLULAR_FAST], 2)
+        self.assertEqual(self.nodes[1][BACTERIUM_EXTRACELLULAR_FAST], 1)
+        self.event.perform()
+        self.assertEqual(self.nodes[0][BACTERIUM_EXTRACELLULAR_FAST], 1)
+        self.assertEqual(self.nodes[1][BACTERIUM_EXTRACELLULAR_FAST], 2)
+
+
 class GetBacteriaTranslocationEventsTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -37,14 +62,17 @@ class GetBacteriaTranslocationEventsTestCase(unittest.TestCase):
                       BloodEdge(self.nodes[0], self.nodes[1])]
         self.network = MetapopulationNetwork(self.nodes, self.edges)
         self.rates_blood = {BACTERIUM_EXTRACELLULAR_FAST: 0.5, BACTERIUM_EXTRACELLULAR_SLOW: 0.6}
-        self.events = get_bacteria_translocation_blood_events([self.nodes[2]], self.rates_blood)
+        self.lymphangitis_rates = {BACTERIUM_EXTRACELLULAR_FAST: 0.7, BACTERIUM_EXTRACELLULAR_SLOW: 0.8}
+        self.events = get_bacteria_translocation_blood_events([self.nodes[2]], self.rates_blood, self.lymphangitis_rates)
 
     def test_events(self):
-        self.assertEqual(len(self.events), 2)
-        for e in self.events:
-            self.assertTrue(isinstance(e, BloodTranslocatePerfusion))
-            self.assertEqual(e.reaction_parameter, self.rates_blood[e._compartment_translocating])
-
+        self.assertEqual(len(self.events), 4)
+        blood = [e for e in self.events if isinstance(e, BloodTranslocatePerfusion)]
+        for b in blood:
+            self.assertEqual(b.reaction_parameter, self.rates_blood[b._compartment_translocating])
+        lymphangitis = [e for e in self.events if isinstance(e, BacteriaTranslocateLymphangitis)]
+        for e in lymphangitis:
+            self.assertEqual(e.reaction_parameter, self.lymphangitis_rates[e._compartment_translocating])
 
 if __name__ == '__main__':
     unittest.main()
